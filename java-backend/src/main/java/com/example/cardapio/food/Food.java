d507fdc261ceff3861a3ff16e72c532ec8f0e084
rike14 <henriquemk00@gmail.com>
2024-12-30 17:46:37 -0300

    init project cardapio with java backend and react with vite and ts frontend

---
+++ java-backend/src/main/java/com/example/cardapio/food/Food.java
@@ -0,0 +1,28 @@
+package com.example.cardapio.food;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "foods")
+@Entity(name = "foods")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Food {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String image;
+
+    private Integer price;
+
+    public Food(FoodRequestDTO data){
+        this.title = data.title();
+        this.image = data.image();
+        this.price = data.price();
+    }
+}
