8a6705a8e6d0830cdbad7b2ef8241af296f71da3
rike14 <henriquemk00@gmail.com>
2025-01-11 14:19:44 -0300

    add test JUnit 5 product requests

---
+++ java-backend/src/test/java/com/example/cardapio/controller/ProductControllerTest.java
@@ -0,0 +1,15 @@
+package com.example.cardapio.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ProductControllerTest {
+
+    @Test
+    void saveProduct() {
+    }
+
+    @Test
+    void getAll() {
+    }
+}
